41cc9aca48061b118d8dbc38b30f04780af8f197
pablo <pabloarg120@gmail.com>
2026-08-24 12:14:39 -0600

    Comentario de invoke

--- app/Http/Controllers/homecontroller.php
+++ app/Http/Controllers/homecontroller.php
@@ -6,7 +6,9 @@ use Illuminate\Http\Request;
 
 class homecontroller extends Controller
 {
-    public function index(){
+    //Invoke solo sirve cuando no tenemos más 'ramas' a quien llamar
+    public function __invoke()
+    {
         return view('home');
     }
 }
