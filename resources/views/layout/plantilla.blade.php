49078b43a3db074daba2f26821ecd97e2cdde4f5
pablo <pabloarg120@gmail.com>
2026-08-28 17:52:32 -0600

    Style: Pie de página con copyright (es mi propiedad jajaja)

--- resources/views/layout/plantilla.blade.php
+++ resources/views/layout/plantilla.blade.php
@@ -37,12 +37,10 @@
     {{-- servirá para que el contenido sea variable a medida que lo nombres en nuestro archivo (contenido) --}}
     @yield('content')
 
-    <footer>
-        <div>
-            <ul>
-                <li>Copyright The Cubick</li>
-            </ul>
+    <footer class="bg-white border-t border-gray-200 py-4">
+        <div class="max-w-7xl mx-auto px-4 text-center text-xs text-gray-500">
+            © 2026 TheCubick. Todos los derechos reservados.
         </div>
-    </footer>
+    </footer>  
 </body>
 </html>
